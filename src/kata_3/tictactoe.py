d4843715652d28e6e16efe686a386fd0bbbbf4de
DANIEL NOVAK <danielnovak444@gmail.com>
2023-01-13 14:54:21 +0100

    refactoring original function random move

--- src/kata_3/tictactoe.py
+++ src/kata_3/tictactoe.py
@@ -3,6 +3,11 @@ import random
 def create_board():
     return [[' ' for _ in range(3)] for _ in range(3)]
 
-def random_move(board):
-    available_moves = [(x, y) for x in range(3) for y in range (3) if board[x][y] == ' ']
-    return random.choice(available_moves)
+def make_move(board,player):
+    available_moves = [(x,y) for x in range(3) for y in range(3) if board[x][y] == ' ']
+    if not available_moves:
+        return None
+    move = random.choice(available_moves)
+    x,y = move
+    board[x][y] = player
+    return move
--- test/test_suite.py
+++ test/test_suite.py
@@ -1,4 +1,4 @@
-from kata_3.tictactoe import create_board, random_move
+from kata_3.tictactoe import create_board, make_move
 import pytest
 
 
@@ -8,8 +8,10 @@ def test_create_board():
     assert all (len(row) == 3 for row in board)
     assert all (all(cell == ' ' for cell in row) for row in board)
 
-def test_random_move():
-    board = [['X', 'O', ' '],
-             [' ', 'X', 'O'],
-             [' ', ' ', 'X']]
-    assert random_move(board) in [(0,2), (1,0), (2,0), (2,1)]
+def test_make_move():
+    board = [[None, None, None],
+             [None, None, None],
+             [None, None, None]]
+    move = make_move(board, 'X')
+    assert move in [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)]
+    assert board[move[0]][move[1]]== 'X'
